483220bb220a35686950471bada90a74a928b332
Sumo2011 <119305784+Sumo2011@users.noreply.github.com>
2023-08-24 21:55:59 +0530

    Add files via upload

---
+++ kk.ino
@@ -0,0 +1,49 @@
+// Includes the Servo library
+#include <Servo.h>. 
+// Defines Tirg and Echo pins of the Ultrasonic Sensor
+const int trigPin = 10;
+const int echoPin = 11;
+//by www.andprof.com
+// Variables for the duration and the distance
+long duration;
+int distance;
+Servo myServo; // Creates a servo object for controlling the servo motor
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  myServo.attach(12);
+}
+void loop() {
+  for(int i=0;i<=180;i++){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  
+  Serial.print(i); 
+  Serial.print(","); 
+  Serial.print(distance); 
+  Serial.print("."); 
+  }
+
+  for(int i=180;i>0;i--){  
+  myServo.write(i);
+  delay(30);
+  distance = calculateDistance();
+  Serial.print(i);
+  Serial.print(",");
+  Serial.print(distance);
+  Serial.print(".");
+  }
+}
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance= duration*0.034/2;
+  return distance;
+}
